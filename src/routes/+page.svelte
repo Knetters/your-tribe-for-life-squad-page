6db1c5cb2e685a91b9562f0f80bb18f505c7a9e1
Tjips <thomas@scheepers.com>
2023-09-06 18:07:05 +0200

    Eerste push (zonder errors)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { SliceZone } from '@prismicio/svelte'
+    import { components } from '$lib/slices' 
+    export let data
+  
+    /** @type {import("@prismicio/client").Content.NamePfSlice} */
+  
+  </script>
+  
+  
+  <section>
+    <SliceZone slices={data.slices} {components} />
+  </section>
