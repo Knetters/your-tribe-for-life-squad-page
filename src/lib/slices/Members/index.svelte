56799031cf801e42cccdd65191c89520d86c8e57
ThomasScheepers <thomas@scheepers.com>
2023-09-08 00:49:01 +0200

    Plaatjes omdraaien

--- src/lib/slices/Members/index.svelte
+++ src/lib/slices/Members/index.svelte
@@ -6,8 +6,12 @@
 <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
 
   {#each slice.items as item (item.slug.text)}
-    <h2>{item.name[0].text}</h2>
     <img src={item.image.url} alt={item.image.alt} />
+    <h2>{item.name[0].text}</h2>
   {/each}
 
 </section>
+
+<style>
+
+</style>
