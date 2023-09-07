346422ac7072e22827e5f2cb0e52fa23e86302e8
Tjips <thomas@scheepers.com>
2023-09-08 00:14:59 +0200

    Het werkt! 😩

--- src/routes/book/[uid]/+page.server.js
+++ src/routes/book/[uid]/+page.server.js
@@ -1,9 +1,9 @@
 import { createClient } from "$lib/prismicio"
 
-export async function load({fetch, request, params}){
+export async function load({fetch, request, params }){
     const client = createClient({fetch, request})
 
-    const document = await client.getByUID('yearbook', params)
+    const document = await client.getByUID('yearbook', params.uid)
 
     return document.data
 }
