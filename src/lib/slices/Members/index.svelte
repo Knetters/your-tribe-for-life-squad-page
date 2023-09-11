744770ce412234bdc34e7709d16aa5776f0a5fd1
ThomasScheepers <thomas@scheepers.com>
2023-09-11 17:09:01 +0200

    Proberen te loopen

--- src/lib/slices/Members/index.svelte
+++ src/lib/slices/Members/index.svelte
@@ -6,6 +6,9 @@
 <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
 
   {#each slice.items as item (item.slug.text)}
+    {#if item.image.url === ""}
+      <img src="/img/placeholder-image.jpg" alt="" />
+    {/if}
     <img src={item.image.url} alt={item.image.alt} />
     <h2>{item.name[0].text}</h2>
   {/each}
