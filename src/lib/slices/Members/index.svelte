198853927bf34f073b346593c654097400ed62f0
Tjips <thomas@scheepers.com>
2023-09-11 17:15:59 +0200

    Werkende loop

--- src/lib/slices/Members/index.svelte
+++ src/lib/slices/Members/index.svelte
@@ -6,15 +6,18 @@
 <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
 
   {#each slice.items as item (item.slug.text)}
-    {#if item.image.url === ""}
-      <img src="/img/placeholder-image.jpg" alt="" />
-    {/if}
-    <img src={item.image.url} alt={item.image.alt} />
+  {#if !item.image.url}
+    <img class="yearbook-image" src="/img/placeholder-image.jpg" alt="" />
+  {:else}
+    <img class="yearbook-image" src={item.image.url} alt={item.image.alt} />
+  {/if}
     <h2>{item.name[0].text}</h2>
   {/each}
 
 </section>
 
 <style>
-
+  .yearbook-image {
+    width: 200px;
+  }
 </style>
